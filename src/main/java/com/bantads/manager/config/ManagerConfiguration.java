104e17e5b809452698e5d482273ef4db13f12685
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-02-11 15:41:40 -0300

    added consumer

---
+++ src/main/java/com/bantads/manager/config/ManagerConfiguration.java
@@ -0,0 +1,48 @@
+package com.bantads.manager.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(RabbitMqConfiguration.class)
+public class ManagerConfiguration {
+
+    static final String createQueueName = "manager.create";
+    static final String updateQueueName = "manager.update";
+    static final String deleteQueueName = "manager.delete";
+
+    @Bean
+    public Queue createQueueCreate() {
+        return new Queue(createQueueName, true);
+    }
+
+    @Bean
+    public Queue updateQueueUpdate() {
+        return new Queue(updateQueueName, true);
+    }
+
+    @Bean
+    public Queue deleteQueueDelete() {
+        return new Queue(deleteQueueName, true);
+    }
+
+    @Bean
+    Binding createBinding(Queue createQueueCreate, DirectExchange exchange) {
+        return BindingBuilder.bind(createQueueCreate).to(exchange).with(createQueueName);
+    }
+
+    @Bean
+    Binding updateBinding(Queue updateQueueUpdate, DirectExchange exchange) {
+        return BindingBuilder.bind(updateQueueUpdate).to(exchange).with(updateQueueName);
+    }
+
+    @Bean
+    Binding deleteBinding(Queue deleteQueueDelete, DirectExchange exchange) {
+        return BindingBuilder.bind(deleteQueueDelete).to(exchange).with(deleteQueueName);
+    }
+}
